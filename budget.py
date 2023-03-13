a0541c19b5a480418f20fd6c9cc7112664b41c71
Trepix <pau.trepat.segura@gmail.com>
2023-03-13 18:13:40 +0100

    Implement when try to withdraw money but there are not enough funds

--- budget.py
+++ budget.py
@@ -3,12 +3,17 @@ class Category:
     def __init__(self, name):
         self.name = name
         self.ledger = []
+        self.total_amount = 0
         pass
 
     def deposit(self, amount, description="") -> None:
         self.ledger.append({"amount": amount, "description": description})
+        self.total_amount += amount
 
     def withdraw(self, amount, description="") -> bool:
+        if self.total_amount - amount < 0:
+            return False
+        
         self.ledger.append({"amount": -amount, "description": description})
         return True
 
