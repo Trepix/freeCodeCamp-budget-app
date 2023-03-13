db63fbe4a19cb8f044b27af6f38530d66525fcdf
Trepix <pau.trepat.segura@gmail.com>
2023-03-13 17:49:15 +0100

    Complete first case, add a deposit to ledger

--- budget.py
+++ budget.py
@@ -1,7 +1,13 @@
 class Category:
 
+    def __init__(self, name):
+        self.name = name
+        self.ledger = []
+        pass
 
-
+    def deposit(self, amount, description):
+        self.ledger.append({"amount": amount, "description": description})
 
 
 def create_spend_chart(categories):
+    pass
