e5367b28b3c3a3ce2ab88c94356388a0360629e8
Trepix <pau.trepat.segura@gmail.com>
2023-03-13 17:53:41 +0100

    Cover with dummy implementation when ledger has enough founds. Add type hinting of return types

--- budget.py
+++ budget.py
@@ -5,11 +5,12 @@ class Category:
         self.ledger = []
         pass
 
-    def deposit(self, amount, description=""):
+    def deposit(self, amount, description="") -> None:
         self.ledger.append({"amount": amount, "description": description})
 
-    def withdraw(self, amount, description=""):
+    def withdraw(self, amount, description="") -> bool:
         self.ledger.append({"amount": -amount, "description": description})
+        return True
 
 
 def create_spend_chart(categories):
